b677f7b596af763ed03260c2653870ba54396612
MutsuroTatsuki <mutsuro314+github@gmail.com>
2019-09-08 19:00:39 +0900

    main文中の変数を整理など

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ JointMotor<PwmOut> motor_theta(&pwm_theta, &enc_theta, 1);
 JointMotor<PwmOut> motor_phi(&pwm_phi, &enc_phi, 1);
 
 Timer pid_timer;
+Timer timer;
 
 
 void led_all(int onoff)
@@ -36,47 +37,58 @@ inline bool has_arrived(int cnt_x, int cnt_y, int cnt_z)
 	return ((cnt_x >= WAIT_ARRIVE) && (cnt_y >= WAIT_ARRIVE) && (cnt_z >= WAIT_ARRIVE));
 }
 
+struct Monitor_cartesian
+{
+public:
+	Monitor_cartesian() : cnt_arrive(0), pos_adjust(0) {}
+	int cnt_arrive;
+	float pos_now;
+	float pos_adjust;
+};
+
+struct Monitor_polar
+{
+public:
+	Monitor_polar(float kp, float ki, float kd) {
+		pid_gain[0] = kp;
+		pid_gain[1] = ki;
+		pid_gain[2] = kd;
+		duty = 0.5;
+	}
+	float pid_gain[3];
+	float pos_now;
+	float pos_next;
+	float duty;
+};
+
 
 int main(){
-	led_all(1);
+	Monitor_cartesian x;
+	Monitor_cartesian y;
+	Monitor_cartesian z;
 
-	float pid_gain_r[3] 	= {0, 0, 0};
-	float pid_gain_theta[3] = {0, 0, 0};
-	float pid_gain_phi[3] 	= {0, 0, 0};
+	Monitor_polar r(0, 0, 0);
+	Monitor_polar theta(0, 0, 0);
+	Monitor_polar phi(0, 0, 0);
 
-	int x_cnt_arrive = 0;
-	int y_cnt_arrive = 0;
-	int z_cnt_arrive = 0;
+	float now_t;
 
-	float duration;
 	Queue<Instruction> queue_inst;
 	Queue<Instruction> queue_buff;
 
-	float r_next;
-	float theta_next;
-	float phi_next;
-
-	float x_now;
-	float y_now;
-	float z_now;
-
-	// pspadの入力で微調整する量
-	float x_adjust;
-	float y_adjust;
-	float z_adjust;
-
 	// 指令をqueueに入れ込む
-	duration = 1;
-	Instruction inst(INIT_X, INIT_Y, INIT_Z, duration,
+	Instruction inst(INIT_X, INIT_Y, INIT_Z, 1,
 			Mode::Init, Mode::Polar, Mode::NonLinearAcc, Mode::Release, Mode::Backward);
 	queue_inst.push(inst);
+
+	led_all(1);
 
 	wait_ms(300);
 	pc.baud(921600);
 
-	motor_r.pid_setting(pid_gain_r, &pid_timer);
-	motor_theta.pid_setting(pid_gain_theta, &pid_timer);
-	motor_phi.pid_setting(pid_gain_phi, &pid_timer);
+	motor_r.pid_setting(r.pid_gain, &pid_timer);
+	motor_theta.pid_setting(theta.pid_gain, &pid_timer);
+	motor_phi.pid_setting(phi.pid_gain, &pid_timer);
 
 	inst = queue_inst.front();
 	catcher.restart(inst.x, inst.y, inst.z);
@@ -89,35 +101,39 @@ int main(){
 	while(1){
 		AdjustCycle(1000);
 
+		now_t = timer.read();
+
 		// 指令に従って次の位置を計算
 		catcher.calc_next();
 
-		r_next = catcher.get_r_next();
-		theta_next = catcher.get_theta_next();
-		phi_next = catcher.get_phi_next();
+		r.pos_next = catcher.get_r_next();
+		theta.pos_next = catcher.get_theta_next();
+		phi.pos_next = catcher.get_phi_next();
 
 		// 次の位置に移動
-		motor_r.move_to(r_next);
-		motor_theta.move_to(theta_next);
-		motor_phi.move_to(phi_next);
-
+		r.duty = motor_r.move_to(r.pos_next);
+		theta.duty = motor_theta.move_to(theta.pos_next);
+		phi.duty = motor_phi.move_to(phi.pos_next);
 		// y方向スライド
 		slider.write(inst.slider);
-
 		// ハンドのサーボ
-		servo.keep(theta_next);
+		servo.keep(theta.pos_next);
 
-		polar2cartesian(motor_r.get_now(), motor_theta.get_now(), motor_phi.get_now(),
-				&x_now, &y_now, &z_now);
-		x_now += X_OFFSET;
-		y_now += Y_OFFSET;
-		z_now += Z_OFFSET;
+		// 現在値取得
+		r.pos_now = motor_r.get_now();
+		theta.pos_now = motor_theta.get_now();
+		phi.pos_now = motor_phi.get_now();
+		polar2cartesian(r.pos_now, theta.pos_now, phi.pos_now,
+				&(x.pos_now), &(y.pos_now), &(z.pos_now));
+		x.pos_now += X_OFFSET;
+		y.pos_now += Y_OFFSET;
+		z.pos_now += Z_OFFSET;
 
-		x_cnt_arrive = counter_update(x_cnt_arrive, x_now, inst.x, BUFF_ARRIVE);
-		y_cnt_arrive = counter_update(y_cnt_arrive, y_now, inst.y, BUFF_ARRIVE);
-		z_cnt_arrive = counter_update(z_cnt_arrive, z_now, inst.z, BUFF_ARRIVE);
+		x.cnt_arrive = counter_update(x.cnt_arrive, x.pos_now, inst.x, BUFF_ARRIVE);
+		y.cnt_arrive = counter_update(y.cnt_arrive, y.pos_now, inst.y, BUFF_ARRIVE);
+		z.cnt_arrive = counter_update(z.cnt_arrive, z.pos_now, inst.z, BUFF_ARRIVE);
 
-		if (has_arrived(x_cnt_arrive, y_cnt_arrive, z_cnt_arrive)) {
+		if (has_arrived(x.cnt_arrive, y.cnt_arrive, z.cnt_arrive)) {
 			// リトライに備えて実行済みinstをバッファに保存
 			// 初期位置に戻ったらqueue_buffをクリア
 			if (inst.state == Mode::Init) {
@@ -129,12 +145,12 @@ int main(){
 
 			if (queue_inst.length() > 1) {
 				queue_inst.pop();
-				x_adjust = 0;
-				y_adjust = 0;
-				z_adjust = 0;
-				x_cnt_arrive = 0;
-				y_cnt_arrive = 0;
-				z_cnt_arrive = 0;
+				x.pos_adjust = 0;
+				y.pos_adjust = 0;
+				z.pos_adjust = 0;
+				x.cnt_arrive = 0;
+				y.cnt_arrive = 0;
+				z.cnt_arrive = 0;
 				inst = queue_inst.front();
 				catcher.restart(inst.x, inst.y, inst.z);
 				catcher.set_duration(inst.duration);
@@ -142,9 +158,9 @@ int main(){
 			}
 		}
 
-		if (pc.readable()) {
-			pc.printf("x: %4.0f  y: %4.0f  z: %4.0f  ", x_now, y_now, z_now);
-		}
+		pc.printf("now: %2.2f  ", now_t);
+		pc.printf("x: %4.1f->   y: %4.1f  z: %4.1f  ", x.pos_now, y.pos_now, z.pos_now);
+		pc.printf("\r\n");
 	}
 
 	return 0;
